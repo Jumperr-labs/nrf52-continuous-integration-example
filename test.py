c1363fdff348d25d1feb1294be167d670695e5e3
seryoni <seryoni@gmail.com>
2018-07-01 22:52:52 +0300

    srecord

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ class TestDebouncer(unittest.TestCase):
     def setUp(self):
         # set up the device simulation
         self.v = Vlab(working_directory=".", print_uart=True, token="demo-token-for-ci")
-        self.v.load("BUILD/STM32_Button_Debounce.bin")
+        self.v.load("BUILD/BLE_HeartRate-combined.hex")
         self.v.on_pin_level_event(self.pin_listener)
         # running for a while to get the BSP done
         self.v.run_for_ms(10)
